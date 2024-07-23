f7fd084fd09ce3579ad94c5af2c5aab31fe14351
Xolaly-1 <133373098+Xolaly-1@users.noreply.github.com>
2024-07-23 20:32:23 +0000

    Add files via upload

---
+++ smbi_curr.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Steadfastness Missions - Online B-School</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!--<link href="../assets/img/favicon.icon" rel="icon">-->
+	<link href="../assets/img/favicon.ico" rel="icon">
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+	
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+       <a href="#" class="logo mr-auto"><img src="../assets/img/logo.png" alt="" class="img-fluid"></a>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="../index.php">Home</a></li>
+
+          <li class="drop-down"><a href="../about/about_us.php">About Us</a>
+            <ul>
+              <li><a href="../about/mission_statement.php">Mission Statement</a></li>
+              <li><a href="../about/faith_statement.php">Faith Statement</a></li>
+			  <li><a href="../about/core_statement.php">Core Statements</a></li>
+			  <li><a href="../about/vision_statement.php">Vision Statement</a></li>
+			  <li><a href="../about/excutive.php">Executive Director</a></li>
+            </ul>
+          </li>
+
+               <li class="drop-down"><a href="../mission/mission.php">Mission</a>
+                <ul>
+                  <li class="drop-down"><a href="../mission/church_planting.php">Church Planting</a>
+					<ul>
+                  <li><a href="../mission/support_mission.php">Support for Mission</a></li>
+                  <li><a href="../mission/join_chariot.php">Join This Chariot</a></li>
+                </ul>
+				</li>
+					
+                  <li><a href="../mission/bibleschool.php">Bible School</a></li>
+                  <li><a href="../mission/future.php">Future</a></li>
+                </ul>
+				   
+		       <li class="drop-down"><a href="../services/services.php">Services</a>
+			    <ul>
+                  <li><a href="../services/activities.php">Activities</a></li>
+                  <li><a href="../services/seminar.php">Seminar</a></li>
+				   </ul>
+			   </li>
+			
+          <li><a href="../support/support.php">Support</a></li>
+          <li><a href="../contact/contact.php">Contact</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+      <div class="header-social-links">
+        <a href="https://twitter.com/" target="_blank" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="https://web.facebook.com/profile.php?id=100078663666498"  target="_blank" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="https://www.instagram.com/apostledavid.olawole" target="_blank" class="instagram"><i class="icofont-instagram"></i></a>
+        <a href="https://www.linkedin.com/in/steadfastness-missions-2788a4232?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bsq1lsUuFQQ61c8bYXuow4g%3D%3D" target="_blank" class="linkedin"><i class="icofont-linkedin"></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Online Services</h2>
+          <ol>
+            <li><a href="../services/services.php">Services</a></li>
+            <li>Online Services</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="row mt-5 justify-content-center" data-aos="fade-up">
+          <div class="col-lg-10">
+ <center><p><strong>BE TRAINED AND EQUIPPED FOR THE WORK OF THE MINISTRY </strong></p>
+   <p><strong>STEADFASTNESS MISSIONS BIBLE INSTITUTE </strong></p>
+   <p>(For Pastors, Evangelists, Missionaries and all Church Leaders]</p>
+    <p><strong>A CITADEL OF THEOLOGICAL / MISSION  KNOWLEDGE </strong></p></center>
+    <p><strong>COURSES:</strong></p>
+	 <p><u>These are the curriculum for Certificate:</u><br>
+     Spiritual Warfare, Dynamics of faith, The Call of God, Ministry of Intercession, Mobilizing funds for your Ministry, Principles of Christian Leadership, Homiletics, Understanding Ministry &amp; Gifts of the Spirit, Evangelism and Discipleship, Principles of Management, Strategies of Church Planting, Church Growth, Practical, Bible Study Helps, The Application Principle, and many more.</p>
+      
+    <p><strong>FACULTY: </strong></p>
+ <p>SMBI, Faculty is made up of highly qualified men &amp; women of God filled   with the Spirit of God active in ministering and trained in teaching and ministering the word of God.</p>
+      
+   <p><strong>BENEFITS:</strong></p>
+  <ol>
+ <li>Assistance in developing your ministry.</li>
+ <li>Ordination and employment for successful candidates</li>
+ <li>Diploma for successful candidate during a graduation ceremony (Sactisfied Practical)</li>
+ <li>Free tuition fees, etc.</li>
+ <li>Links with international organizations.</li> 
+  </ol>
+                  
+  <p><strong>SCHEDULES OF PROGRAMME</strong></p>
+    <ol>
+   <li>Evening classes only 5:30pm – 8:30 pm (3 days in a week). NB: It can reschedule</li>
+   <li>Saturday 8:30am – 3:30pm.</li>
+   <li>Special arrangement to be made for candidate at remote area.</li>
+   <li>Classes begin by September.</li>
+   <!--<li>Duration is six months.</li>-->
+	</ol><br />
+   <p><a href="online_bibleschool.php" style="background-color: #4CAF50; border: #7E0507; border-radius: 8px; color: white; padding: 10px 7px; text-align: center; text-decoration: none; display: inline-block; margin: 2px 0px; font-size: 12px; cursor: pointer;">Next &nbsp;</a> 
+   <!--<a href="../forms/donation_data/donationi_form.php" style="background-color: #4CAF50; border: #7E0507; border-radius: 8px; color: white; padding: 10px 7px; text-align: center; text-decoration: none; display: inline-block; margin: 2px 0px; font-size: 12px; cursor: pointer;">Donate Now</a></p>-->
+
+          
+		  </div>
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Steadfastness Missions Inc.</h4>
+            <p>
+              Adjacent Bankyease,<br>
+              Amanfrom, Pokuase,<br>
+              Accra-North, Ghana.<br><br>
+				 
+              <strong>Phone:</strong><br> 
+			  +233 201 191 925<br>+233 592 140 092<br><br>
+          
+			  <strong>Email:</strong><br>
+              hope@steadfastnessmissions.org<br>steadfastnessmissions@gmail.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Bible Teachings</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="../doctrine/theology/theology.php">Theology</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../doctrine/ministry/ministry.php">Ministry</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../doctrine/marriage/marriage.php">Marriage</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../doctrine/anthropology/anthropology.php">Anthropology</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../doctrine/salvation/salvation.php">Salvation</a></li>
+			  <li><i class="bx bx-chevron-right"></i> <a href="../doctrine/endtime/endtime.php">End Time</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../doctrine/etiquette/etics.php">Biblical Etiquette</a></li>
+             <!--<li><i class="bx bx-chevron-right"></i> <a href="../doctrine/others/others.php">others</a></li>-->
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>General</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="../general/prayer.php">Prayer Points</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../gal/galaries.php">Galary</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="../stv/stv.php">Steadfatness TV</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#" title="Coming Soon">Store</a></li>
+              <!--<li><i class="bx bx-chevron-right"></i> <a href="#"></a></li>
+			  <li><i class="bx bx-chevron-right"></i> <a href="#"></a></li>
+			  <li><i class="bx bx-chevron-right"></i> <a href="#"></a></li>
+			  <li><i class="bx bx-chevron-right"></i> <a href="#">Store</a></li>-->
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4 align="center" style="color: #44F072">Join The Chariot</h4>
+            <p align="center"><marquee>You can click to join the Chariot before it leaves!!!</marquee></p>
+            <a href="../mission/join_chariot.php" align="center"><img src="../assets/img/join_chariot.jpg" alt="Chariot Image" width="450" height="300"></a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="mr-md-auto text-center text-md-left">
+        <div class="copyright">
+         Copyright &copy; <?php echo date("Y");?>. <span style="font-weight: bold">STEADFASTNESS MISSIONS INC.</span>. All Rights Reserved.
+        </div>
+        <div class="credits">
+          Designed By <a href="https://www.solateck.com/" title="+233201191925" target="_blank" >Solateck</a>
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="https://twitter.com/" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="https://web.facebook.com/profile.php?id=100078663666498"  target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://www.instagram.com/apostledavid.olawole" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <!--<a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>-->
+        <a href="https://www.linkedin.com/in/steadfastness-missions-2788a4232?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bsq1lsUuFQQ61c8bYXuow4g%3D%3D" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../assets/vendor/jquery/jquery.min.js"></script>
+  <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="../assets/vendor/php-email-form/validate.js"></script>
+  <script src="../assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="../assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="../assets/vendor/venobox/venobox.min.js"></script>
+  <script src="../assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="../assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="../assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../assets/js/main.js"></script>
+
+</body>
+
+</html>
